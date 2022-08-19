a0880fbf10795a674474060d85f67d92456de60a
Blackflame6 <engineerfranklyn@gmail.com>
2022-08-19 09:34:45 -0500

    penultimate commit

--- httpServer.js
+++ httpServer.js
@@ -3,24 +3,24 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer();
-
+console.log(module)
 const PORT = 8000;
 
-server.on("request", (req, res) => {
-  fs.readFile("pets.json", "utf-8", (error, data) => {
-    if (error) {
-      console.log("We hit a snag");
-    } 
-    if (req.url === "/pets") {
-      res.status == 200;
-      res.setHeader("Content-Type", "text/plain");
-      res.end(data);
-    }
-
+server.on('request', (req, res)=> {
+  fs.readFile('pets.json', 'utf-8', (error, data) => {
+    
     const pet = req.url.split('/')
     const index = pet[pet.length -1]
-    if(req.url === `/pets/${index}`)  {
+    if(req.url === `/pets/${index}` && ( index >=2 || index <= -1) ) {
     
+      res.status = 404
+      res.setHeader("Content-Type", "text/plain");
+     console.log(req.url)
+      res.end(`Not Found`)
+  
+      
+    } else if (req.url === `/pets/${index}`){
+
       res.status = 200
       res.setHeader("Content-Type", "text/plain");
      
@@ -30,12 +30,25 @@ server.on("request", (req, res) => {
      
       res.end(petString)
   
+    }
+    
+    if (error) {
+      console.log("We hit a snag");
+    } 
+    if (req.url === "/pets") {
+      res.status == 200;
+      res.setHeader("Content-Type", "text/plain");
+      console.log(req.url)
+      res.end(data);
       
     }
+
+
   });
   
 
 });
+
 
 server.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
